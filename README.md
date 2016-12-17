a2b9d47ec5e6a5b90d00ba3f18b0bfd881548954
Sharjeel-Khan <smkhan@qatar.cmu.edu>
2016-12-17 18:56:04 -0500

    Added STL feature and Image Gallery

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
-## Copyright and License
+You can check the Portfolio at http://www.smkhan.me
 
+## Copyright and License
 Copyright 2013-2016 Blackrock Digital LLC. Code released under the [MIT](https://github.com/BlackrockDigital/startbootstrap-freelancer/blob/gh-pages/LICENSE) license.
--- js/portfolioController.js
+++ js/portfolioController.js
@@ -7,36 +7,40 @@
                 name: "Kesa",
                 id: "portfolioModal1",
                 filter: "web-development",
-                image: "img/portfolio/KESA.png",
                 description: "Kesa is a web application used to write and read branching storylines stories collaboratively. You can create and allow your friends to help you write a story with branching storylines. After you are done, you can publish it and read it while making choices.",
                 libraries: "The tree structure and visualization is built using D3 while the collaborative sessions were maintained by PeerJS which used a deployed server on heroku for communication. The frontend was done using AngularJS while the backend was done using Python's Django.",
                 purpose: "Web Application and Development Course",
                 date: "April - August 2016",
                 service: "Web Development",
+                images: ["KESA1.png","KESA2.png"],
+                imageindex: 0,
                 show: true
               },
               {
                 name: "Embedded Systems: MC9S12C128",
                 id: "portfolioModal2",
                 filter: "embedded-systems",
-                image: "img/portfolio/EmbeddedSystems.jpg",
                 description: "I used a MC9S12C128 board where we coded all the programs in C and Assembly with input/output devices.",
                 libraries: "",
                 purpose: "Embedded Systems Course",
                 date: "Jan - May 2016",
                 service: "Embedded Systems",
+                images: ["EmbeddedSystems.jpg"],
+                imageindex: 0,
                 show: true
               },
               {
                 name: "Pascaline",
                 id: "portfolioModal3",
                 filter: "3d-modeling",
-                image: "img/portfolio/Pascaline.png",
                 description: "In Solidworks, I created 3D models of a Pascaline which is a mechanical calculator invented by Pascal in the early 17th century.",
                 libraries: "",
                 purpose: "Rapid Prototyping Technologies Course",
                 date: "September - December 2016",
                 service: "3D Printing and Laser Cutting",
+                images: ["Pascaline1.png","Pascaline2.jpg","Pascaline3.jpg","Pascaline4.jpg",
+                         "Pascaline5.jpg","Pascaline6.jpg","Pascaline7.jpg"],
+                imageindex: 0,
                 show: true
               }
             ];
@@ -68,9 +72,9 @@
                   show: true
               },
               {
-                  name: "NodeJS",
+                  name: "Django",
                   filter: "web",
-                  percentage: "70",
+                  percentage: "85",
                   show: true
               },
               {
@@ -86,21 +90,21 @@
                   show: true
               },
               {
-                  name: "Django",
+                  name: "NodeJS",
                   filter: "web",
-                  percentage: "85",
+                  percentage: "70",
                   show: true
               },
               {
-                  name: "C",
+                  name: "Python",
                   filter: "programming",
-                  percentage: "80",
+                  percentage: "95",
                   show: false
               },
               {
                   name: "Java",
                   filter: "programming",
-                  percentage: "90",
+                  percentage: "85",
                   show: false
               },
               {
@@ -116,9 +120,9 @@
                   show: false
               },
               {
-                  name: "Adobe Creative Cloud",
-                  filter: "apps",
-                  percentage: "70",
+                  name: "C",
+                  filter: "programming",
+                  percentage: "80",
                   show: false
               },
               {
@@ -137,6 +141,18 @@
                   name: "Solidworks",
                   filter: "apps",
                   percentage: "85",
+                  show: false
+              },
+              {
+                  name: "Vim",
+                  filter: "apps",
+                  percentage: "80",
+                  show: false
+              },
+              {
+                  name: "Adobe Indesign",
+                  filter: "apps",
+                  percentage: "70",
                   show: false
               },
             ];
@@ -184,6 +200,20 @@
                 else {
                   this.skills[i].show = false;
                 }
+              }
+            };
+
+            this.nextImage = function(project) {
+              if(project.imageindex < project.images.length - 1)
+              {
+                project.imageindex += 1;
+              }
+            };
+
+            this.prevImage = function(project) {
+              if(project.imageindex > 0)
+              {
+                project.imageindex -= 1;
               }
             };
         });
